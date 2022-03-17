f3069dcfae0cc209ee73e07f842ae997605ab08b
rancid97 <damian.ertel5@gmail.com>
2022-03-17 23:00:35 +0100

    add mongodb

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://rancid:password@mongo:27017/?authSource=admin')
+    .then(() => console.log("MONGO DB CONNECTED"))
+    .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send("<h2>Hello There!!!</h2>")
@@ -8,4 +13,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3002
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`));
+
